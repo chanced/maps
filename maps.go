f2c425be6c067e3156688723b28a0050f55e8dc6
Chance <chanceusc@gmail.com>
2022-09-02 12:32:14 -0400

    init

---
+++ maps.go
@@ -0,0 +1,41 @@
+package maps
+
+import (
+	"sort"
+
+	"golang.org/x/exp/constraints"
+)
+
+type sortable[T constraints.Ordered] []T
+
+// Len implements sort.Interface
+func (x sortable[T]) Len() int               { return len(x) }
+func (x sortable[T]) Less(i int, j int) bool { return x[i] < x[j] }
+func (x sortable[T]) Swap(i int, j int)      { x[i], x[j] = x[j], x[i] }
+
+var _ sort.Interface = sortable[uint8]{}
+
+func SortKeys[K constraints.Ordered, V any](m map[K]V) []K {
+	keys := make([]K, len(m))
+	i := 0
+	for k := range m {
+		keys[i] = k
+		i += 1
+	}
+	sort.Sort(sortable[K](keys))
+	return keys
+}
+
+type KV[K constraints.Ordered, V any] struct {
+	K K
+	V V
+}
+
+func Sort[K constraints.Ordered, V any](m map[K]V) []KV[K, V] {
+	sorted := make([]KV[K, V], len(m))
+	keys := SortKeys(m)
+	for i, k := range keys {
+		sorted[i] = KV[K, V]{k, m[k]}
+	}
+	return sorted
+}
