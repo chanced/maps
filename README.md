0d3bcc43433c698daafcbd62d2d921913bdd97c0
Chance <chanceusc@gmail.com>
2022-09-02 12:35:36 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# maps is a (small) map utilities library
+
+If you need a sorted map and need performance, consider a [btree](https://github.com/google/btree).
+
+## License
+
+MIT
--- maps.go
+++ maps.go
@@ -15,6 +15,7 @@ func (x sortable[T]) Swap(i int, j int)      { x[i], x[j] = x[j], x[i] }
 
 var _ sort.Interface = sortable[uint8]{}
 
+// SortKeys sorts the keys of a map.
 func SortKeys[K constraints.Ordered, V any](m map[K]V) []K {
 	keys := make([]K, len(m))
 	i := 0
@@ -26,11 +27,13 @@ func SortKeys[K constraints.Ordered, V any](m map[K]V) []K {
 	return keys
 }
 
+// KV is a Key Value pair.
 type KV[K constraints.Ordered, V any] struct {
 	K K
 	V V
 }
 
+// Sort sorts a map by key, returning a slice of KV structs.
 func Sort[K constraints.Ordered, V any](m map[K]V) []KV[K, V] {
 	sorted := make([]KV[K, V], len(m))
 	keys := SortKeys(m)
